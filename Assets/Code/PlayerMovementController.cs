fe3eb577f1fc4f4e5084a3e431fcc29937ea9617
sim-mcqueen <s.mcqueen@digipen.edu>
2021-03-25 14:12:25 -0700

    Revert "Revert "Spade Card + Ammo Counter""
    This reverts commit aaad2c0c9b359819a280625caceeb9222329123b.

--- Assets/Code/PlayerMovementController.cs
+++ Assets/Code/PlayerMovementController.cs
@@ -33,6 +33,7 @@ public class PlayerMovementController : MonoBehaviour
     private float startingX = 0;
     private PlayerAnimationManager animationManager;
     private float invulnerableTime = -5f;
+    private int SpadeAmmo = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -167,6 +168,12 @@ public class PlayerMovementController : MonoBehaviour
         if(collision.collider.gameObject.CompareTag("JokerCard"))
         {
             invulnerableTime = Time.time;
+            Destroy(collision.collider.gameObject);
+        }
+        if (collision.collider.gameObject.CompareTag("Spade"))
+        {
+            SpadeAmmo += 1;
+            print("Spade Ammo: " + SpadeAmmo);
             Destroy(collision.collider.gameObject);
         }
 
