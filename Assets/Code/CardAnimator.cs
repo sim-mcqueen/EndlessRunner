ae696ac07f59062bce70dc5e5e0d876ec1853a5e
sim-mcqueen <s.mcqueen@digipen.edu>
2021-03-30 17:22:10 -0700

    Added card animations and textures

---
+++ Assets/Code/CardAnimator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CardAnimator : MonoBehaviour
+{
+    public Sprite[] frames;
+    public SpriteRenderer card;
+    public float frameRate = 0.1f;
+
+    private int currentImage;
+    // Use this for initialization
+    void Start()
+    {
+        currentImage = 0;
+        InvokeRepeating("ChangeImage", 0.1f, frameRate);
+    }
+
+    private void ChangeImage()
+    {
+        if (currentImage == frames.Length - 1)
+        {
+            currentImage = 0;
+        }
+        currentImage += 1;
+        if (currentImage <= frames.Length - 1)
+        {
+            card.sprite = frames[currentImage];
+        }
+    }
+}
