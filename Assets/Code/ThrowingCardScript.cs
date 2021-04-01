93fe54b206e9a8d114fa5576df5a4deeefb8bec2
adahlmanoeth <71671986+andrew-dahlmanoeth@users.noreply.github.com>
2021-04-01 13:38:30 -0700

    Changed projectile script
    Added a card sprite and made it rotate

--- Assets/Code/ThrowingCardScript.cs
+++ Assets/Code/ThrowingCardScript.cs
@@ -6,10 +6,16 @@ public class ThrowingCardScript : MonoBehaviour
 {
     public float speed = 20f;
     public Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
         rb.velocity = transform.right * speed;
+    }
+    
+    void FixedUpdate()
+    {
+	    gameObject.transform.Rotate(0, 0, +5); //Rotate(x, y, z) + = add one per update
     }
 
     void OnTriggerEnter2D(Collider2D hitInfo)
